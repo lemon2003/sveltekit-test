5517d86cde06d08d785a465381ec09a950c1118d
wasabi <Lemon2003_2@outlook.jp>
2022-06-25 21:40:25 +0900

    [add] app header

---
+++ public/src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import "$styles/global.scss";
+  import "./_layout/style.scss";
+</script>
+
+<div class="appRoot">
+  <div class="appHeader">
+    <div class="appHeader-inner">
+      <div class="appHeader-group">
+        <a href="/"><span>top page</span></a>
+      </div>
+      <div class="appHeader-group">
+        <a href="/search"><span>search</span></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="appBody">
+    <slot />
+  </div>
+
+  <div class="appFooter">
+    <div class="appFooter-inner" />
+  </div>
+</div>
