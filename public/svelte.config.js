354a1c024b7362aec8af402c0539835b4976e7db
wasabi <Lemon2003_2@outlook.jp>
2022-06-25 20:36:37 +0900

    [claify] format code

--- public/svelte.config.js
+++ public/svelte.config.js
@@ -1,27 +1,27 @@
 import path, { resolve } from "path";
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		aliases: {
-			"$styles": path.resolve("./src/styles")
-		}
-	}),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    aliases: {
+      $styles: path.resolve("./src/styles")
+    }
+  }),
 
-	kit: {
-		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					$styles: resolve("./src/styles")
-				}
-			}
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          $styles: resolve("./src/styles")
+        }
+      }
+    }
+  }
 };
 
 export default config;
