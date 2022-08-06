9501c04af39366bd870aa93e1c26f1c1161c447c
wasabi <Lemon2003_2@outlook.jp>
2022-08-06 19:25:08 +0900

    [add] sidebar - change max-width and split into  variable

--- public/src/routes/articles/[slug]/index.svelte
+++ public/src/routes/articles/[slug]/index.svelte
@@ -18,17 +18,34 @@
 </div>
 
 <div class="articleBody">
-  <!-- prettier-ignore -->
-  <main class="articleBody__inner">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, reiciendis.
-    Aliquam, accusamus laborum. Quam porro repellendus, quibusdam magnam sed accusantium.
-    Debitis voluptates culpa, sunt quaerat ea rem harum in nihil!
-    Quae assumenda omnis qui aliquam commodi expedita facilis beatae minus.
-    Corporis, id voluptate sint ipsam ad eos laboriosam aperiam ut.
-    Nobis aliquid officia libero iusto. Autem possimus nesciunt asperiores quas.
-    Inventore blanditiis eligendi repellat in exercitationem quo rem, temporibus minima.
-    Accusantium vel officia, numquam velit praesentium iure fugit quisquam similique?
-    Magni obcaecati placeat, vel praesentium numquam hic dolor ipsum aperiam.
-    Porro consequatur nobis dicta voluptatibus omnis atque consequuntur est itaque?
-  </main>
+  <div class="articleBody__inner">
+    <!-- prettier-ignore -->
+    <div class="articleContent">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, reiciendis.
+      Aliquam, accusamus laborum. Quam porro repellendus, quibusdam magnam sed accusantium.
+      Debitis voluptates culpa, sunt quaerat ea rem harum in nihil!
+      Quae assumenda omnis qui aliquam commodi expedita facilis beatae minus.
+      Corporis, id voluptate sint ipsam ad eos laboriosam aperiam ut.
+      Nobis aliquid officia libero iusto. Autem possimus nesciunt asperiores quas.
+      Inventore blanditiis eligendi repellat in exercitationem quo rem, temporibus minima.
+      Accusantium vel officia, numquam velit praesentium iure fugit quisquam similique?
+      Magni obcaecati placeat, vel praesentium numquam hic dolor ipsum aperiam.
+      Porro consequatur nobis dicta voluptatibus omnis atque consequuntur est itaque?
+    </div>
+
+    <div class="articleSidebar">
+      <ul>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Iusto, corporis quidem!</li>
+        <li>Hic, sequi sint.</li>
+        <li>Numquam, libero beatae!</li>
+        <li>Exercitationem, nesciunt ipsum.</li>
+        <li>Excepturi, quisquam voluptates.</li>
+        <li>Facere, laborum placeat.</li>
+        <li>Illo, corrupti enim.</li>
+        <li>Consectetur, vel quam.</li>
+        <li>In, temporibus optio?</li>
+      </ul>
+    </div>
+  </div>
 </div>
