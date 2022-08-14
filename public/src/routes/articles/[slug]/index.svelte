c26282fda6edf1e2783ffbfb5b89034934ef76ea
wasabi <Lemon2003_2@outlook.jp>
2022-08-14 20:32:16 +0900

    [change] article page layout

--- public/src/routes/articles/[slug]/index.svelte
+++ public/src/routes/articles/[slug]/index.svelte
@@ -8,32 +8,31 @@
   };
 </script>
 
-<div class="articleHeader">
-  <div class="articleHeader__inner">
-    <h1 class="articleTitle">{article.title}</h1>
-    <ul class="articleMeta">
-      <li class="articleMeta__item"><span class="c-icon">history</span>{article.date}</li>
-    </ul>
+<div class="articleWrapper">
+  <div class="articleBody">
+    <header class="articleHeader">
+      <h1 class="articleTitle">{article.title}</h1>
+      <ul class="articleMeta">
+        <li class="articleMeta__item"><span class="c-icon">history</span>{article.date}</li>
+      </ul>
+    </header>
+    <main class="articleMain">
+      <article class="articleContent">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, reiciendis.
+        Aliquam, accusamus laborum. Quam porro repellendus, quibusdam magnam sed accusantium.
+        Debitis voluptates culpa, sunt quaerat ea rem harum in nihil!
+        Quae assumenda omnis qui aliquam commodi expedita facilis beatae minus.
+        Corporis, id voluptate sint ipsam ad eos laboriosam aperiam ut.
+        Nobis aliquid officia libero iusto. Autem possimus nesciunt asperiores quas.
+        Inventore blanditiis eligendi repellat in exercitationem quo rem, temporibus minima.
+        Accusantium vel officia, numquam velit praesentium iure fugit quisquam similique?
+        Magni obcaecati placeat, vel praesentium numquamhic dolor ipsum aperiam.
+        Porro consequatur nobis dicta voluptatibus omnis atque consequuntur est itaque?
+      </article>
+    </main>
   </div>
-</div>
-
-<div class="articleBody">
-  <div class="articleBody__inner">
-    <!-- prettier-ignore -->
-    <div class="articleContent">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, reiciendis.
-      Aliquam, accusamus laborum. Quam porro repellendus, quibusdam magnam sed accusantium.
-      Debitis voluptates culpa, sunt quaerat ea rem harum in nihil!
-      Quae assumenda omnis qui aliquam commodi expedita facilis beatae minus.
-      Corporis, id voluptate sint ipsam ad eos laboriosam aperiam ut.
-      Nobis aliquid officia libero iusto. Autem possimus nesciunt asperiores quas.
-      Inventore blanditiis eligendi repellat in exercitationem quo rem, temporibus minima.
-      Accusantium vel officia, numquam velit praesentium iure fugit quisquam similique?
-      Magni obcaecati placeat, vel praesentium numquam hic dolor ipsum aperiam.
-      Porro consequatur nobis dicta voluptatibus omnis atque consequuntur est itaque?
-    </div>
-
-    <div class="articleSidebar">
+  <aside class="articleSidebar">
+    <nav>
       <ul>
         <li>Lorem, ipsum dolor.</li>
         <li>Iusto, corporis quidem!</li>
@@ -46,6 +45,6 @@
         <li>Consectetur, vel quam.</li>
         <li>In, temporibus optio?</li>
       </ul>
-    </div>
-  </div>
+    </nav>
+  </aside>
 </div>
