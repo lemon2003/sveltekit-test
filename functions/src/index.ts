b4dd0c6b2d979195ceb2e23ad2384b4dcff0aede
wasabi <Lemon2003_2@outlook.jp>
2022-08-30 02:16:45 +0900

    [change] set region to asia-northeast1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,4 +5,4 @@ import { handler } from "public/build/handler.js";
 const app = express();
 app.use(handler);
 
-export const ssr = functions.https.onRequest(app);
+export const ssr = functions.region("asia-northeast1").https.onRequest(app);
